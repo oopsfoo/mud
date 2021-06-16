201b04311298090fc8da28952745d84ed4379fae
oopsfoo <oopsfoo@users.noreply.github.com>
2021-06-16 11:18:13 +0800

    add simplelog deps

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use std::os::raw::{c_char, c_long, c_longlong, c_short, c_int, c_ulong, c_ushort};
-use std::ffi::CString;
-use std::io;
-use winapi::shared::ws2def::{SOCKADDR, ADDRESS_FAMILY, AF_INET};
-use winapi::shared::ws2ipdef;
-use std::ptr::slice_from_raw_parts;
 use std::net::{SocketAddrV4, Ipv4Addr};
-use winapi::um::winsock2::{u_char, u_long, u_short, u_int64};
 use winapi::ctypes::c_uchar;
+use winapi::um::winsock2::{u_char, u_long, u_short, u_int64};
+use winapi::shared::ws2def::{SOCKADDR, ADDRESS_FAMILY, AF_INET};
+use std::io::stdin;
+use std::ffi::CString;
 
-pub type ENDPOINT_ID = u_int64;
+#[macro_use] extern crate log;
+extern crate simplelog;
+use simplelog::*;
+
+#[link(name = "nfapi")]
+extern "C" {
+    fn nf_init(driver: *const c_char, handler: &NFEventHandler) -> NFStatus;
+    fn nf_free();
+    fn nf_adjustProcessPriviledges();
+    fn nf_addRule(rule: *const NFRule, to_head: c_int) -> NFStatus;
+    fn nf_tcpPostSend(id: EndpointId, buf: *const c_char, len: c_int);
+    fn nf_tcpPostReceive(id: EndpointId, buf: *const c_char, len: c_int);
+    fn nf_udpPostSend(id: EndpointId, remote_address: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions);
+    fn nf_udpPostReceive(id: EndpointId, remote_address: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions);
+}
+
+type EndpointId = u_int64;
+
+const NF_MAX_ADDRESS_LENGTH: usize = 28;
+const NF_MAX_IP_ADDRESS_LENGTH: usize = 16;
 
 #[repr(C)]
+#[derive(Debug)]
 enum NFStatus {
     NfStatusSuccess = 0,
     NfStatusFail = -1,
@@ -22,46 +40,34 @@ enum NFStatus {
 
 #[repr(C)]
 enum NFFilteringFlag {
-    NF_ALLOW = 0,
-    // Allow the activity without filtering transmitted packets
-    NF_BLOCK = 1,
-    // Block the activity
-    NF_FILTER = 2,
-    // Filter the transmitted packets
-    NF_SUSPENDED = 4,
-    // Suspend receives from server and sends from client
-    NF_OFFLINE = 8,
-    // Emulate establishing a TCP connection with remote server
-    NF_INDICATE_CONNECT_REQUESTS = 16,
-    // Indicate outgoing connect requests to API
-    NF_DISABLE_REDIRECT_PROTECTION = 32,
-    // Disable blocking indicating connect requests for outgoing connections of local proxies
-    NF_PEND_CONNECT_REQUEST = 64,
-    // Pend outgoing connect request to complete it later using nf_complete(TCP|UDP)ConnectRequest
-    NF_FILTER_AS_IP_PACKETS = 128,
-    // Indicate the traffic as IP packets via ipSend/ipReceive
-    NF_READONLY = 256,
-    // Don't block the IP packets and indicate them to ipSend/ipReceive only for monitoring
-    NF_CONTROL_FLOW = 512,
-    // Use the flow limit rules even without NF_FILTER flag
-    NF_REDIRECT = 1024,            // Redirect the outgoing TCP connections to address specified in redirectTo
+    NfAllow = 0,
+    NfFilter = 2,
+    NfSuspended = 4,
+    NfOffline = 8,
+    NfIndicateConnectRequests = 16,
+    NfDisableRedirectProtection = 32,
+    NfPendConnectRequest = 64,
+    NfFilterAsIpPackets = 128,
+    NfReadonly = 256,
+    NfControlFlow = 512,
+    NfRedirect = 1024,
 }
 
-#[repr(C)]
-struct TcpConnInfo {
-    // unsigned long	filteringFlag;	// See NF_FILTERING_FLAG
-    // unsigned long	processId;		// Process identifier
-    // unsigned char	direction;		// See NF_DIRECTION
-    // unsigned short	ip_family;		// AF_INET for IPv4 and AF_INET6 for IPv6
-    // unsigned char	localAddress[NF_MAX_ADDRESS_LENGTH];
-    // unsigned char	remoteAddress[NF_MAX_ADDRESS_LENGTH];
+#[repr(packed)]
+struct NFTcpConnInfo {
+    filtering_flag: c_ulong,
+    process_id: c_ulong,
+    direction: c_uchar,
+    ip_family: c_ushort,
+    local_address: [c_uchar; NF_MAX_ADDRESS_LENGTH],
+    remote_address: [c_uchar; NF_MAX_ADDRESS_LENGTH],
 }
 
-#[repr(C)]
+#[repr(packed)]
 struct NFUdpConnInfo {
     process_id: c_ulong,
     ip_family: c_ushort,
-    local_address: SOCKADDR,
+    local_address: [c_uchar; NF_MAX_ADDRESS_LENGTH],
 }
 
 #[repr(C)]
@@ -74,15 +80,15 @@ struct NFUdpOptions {
 #[repr(packed)]
 struct NFRule {
     protocol: c_int,
-    process_id: u_long,
-    direction: u_char,
-    local_port: u_short,
-    remote_port: u_short,
-    ip_family: u_short,
-    local_ip_address: [u_char; 16],
-    local_ip_address_mask: [u_char; 16],
-    remote_ip_address: [u_char; 16],
-    remote_ip_address_mask: [u_char; 16],
+    process_id: c_ulong,
+    direction: c_uchar,
+    local_port: c_ushort,
+    remote_port: c_ushort,
+    ip_family: c_ushort,
+    local_ip_address: [c_uchar; NF_MAX_IP_ADDRESS_LENGTH],
+    local_ip_address_mask: [c_uchar; NF_MAX_IP_ADDRESS_LENGTH],
+    remote_ip_address: [c_uchar; NF_MAX_IP_ADDRESS_LENGTH],
+    remote_ip_address_mask: [c_uchar; NF_MAX_IP_ADDRESS_LENGTH],
     filtering_flag: NFFilteringFlag,
 }
 
@@ -90,33 +96,22 @@ struct NFRule {
 struct NFEventHandler {
     thread_start: extern fn(),
     thread_end: extern fn(),
-    tcp_connect_request: extern fn(id: c_longlong, conn_info: TcpConnInfo),
-    tcp_connected: extern fn(id: c_longlong, conn_info: TcpConnInfo),
-    tcp_closed: extern fn(id: c_longlong, conn_info: TcpConnInfo),
-    tcp_receive: unsafe extern fn(id: ENDPOINT_ID, buf: *const c_char, len: c_int),
-    tcp_send: unsafe extern fn(id: ENDPOINT_ID, buf: *const c_char, len: c_int),
+    tcp_connect_request: extern fn(id: c_longlong, conn_info: NFTcpConnInfo),
+    tcp_connected: extern fn(id: c_longlong, conn_info: NFTcpConnInfo),
+    tcp_closed: extern fn(id: c_longlong, conn_info: NFTcpConnInfo),
+    tcp_receive: unsafe extern fn(id: EndpointId, buf: *const c_char, len: c_int),
+    tcp_send: unsafe extern fn(id: EndpointId, buf: *const c_char, len: c_int),
     tcp_can_receive: extern fn(id: c_longlong),
     tcp_can_send: extern fn(id: c_longlong),
     udp_created: unsafe extern fn(id: c_longlong, conn_info: *const NFUdpConnInfo),
     udp_connect_request: extern fn(id: c_longlong, conn_info: NFUdpConnInfo),
     udp_closed: extern fn(id: c_longlong, conn_info: NFUdpConnInfo),
-    udp_receive: unsafe extern fn(id: ENDPOINT_ID, remote: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions),
-    udp_send: unsafe extern fn(id: ENDPOINT_ID, remote: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions),
+    udp_receive: unsafe extern fn(id: EndpointId, remote: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions),
+    udp_send: unsafe extern fn(id: EndpointId, remote: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions),
     udp_can_receive: extern fn(id: c_longlong),
     udp_can_send: extern fn(id: c_longlong),
 }
 
-#[link(name = "nfapi")]
-extern "C" {
-    fn nf_init(driver: *const c_char, handler: &NFEventHandler) -> NFStatus;
-    fn nf_free();
-    fn nf_adjustProcessPriviledges();
-    fn nf_addRule(rule: *const NFRule, to_head: c_int) -> NFStatus;
-    fn nf_tcpPostSend(id: ENDPOINT_ID, buf: *const c_char, len: c_int);
-    fn nf_tcpPostReceive(id: ENDPOINT_ID, buf: *const c_char, len: c_int);
-    fn nf_udpPostSend(id: ENDPOINT_ID, remoteAddress: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions);
-    fn nf_udpPostReceive(id: ENDPOINT_ID, remoteAddress: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions);
-}
 
 extern fn nf_thread_start() {
     println!("threadStart();");
@@ -126,24 +121,24 @@ extern fn nf_thread_end() {
     println!("threadEnd();");
 }
 
-extern fn nf_tcp_connect_request(id: c_longlong, conn_info: TcpConnInfo) {
+extern fn nf_tcp_connect_request(id: c_longlong, conn_info: NFTcpConnInfo) {
     println!("tcp->ConnectRequest();");
 }
 
-extern fn nf_tcp_connected(id: c_longlong, conn_info: TcpConnInfo) {
+extern fn nf_tcp_connected(id: c_longlong, conn_info: NFTcpConnInfo) {
     println!("tcp->Connected();");
 }
 
-extern fn nf_tcp_closed(id: c_longlong, conn_info: TcpConnInfo) {
+extern fn nf_tcp_closed(id: c_longlong, conn_info: NFTcpConnInfo) {
     println!("tcp->Closed();");
 }
 
-unsafe extern fn nf_tcp_receive(id: ENDPOINT_ID, buf: *const c_char, len: c_int) {
+unsafe extern fn nf_tcp_receive(id: EndpointId, buf: *const c_char, len: c_int) {
     println!("tcp->receive();");
     nf_tcpPostReceive(id, buf, len);
 }
 
-unsafe extern fn nf_tcp_send(id: ENDPOINT_ID, buf: *const c_char, len: c_int) {
+unsafe extern fn nf_tcp_send(id: EndpointId, buf: *const c_char, len: c_int) {
     println!("tcp->send();");
     nf_tcpPostSend(id, buf, len);
 }
@@ -157,24 +152,24 @@ extern fn nf_tcp_can_send(id: c_longlong) {
 }
 
 unsafe extern fn nf_udp_created(id: c_longlong, conn_info: *const NFUdpConnInfo) {
-    let local_addr: SOCKADDR = (*conn_info).local_address;
-    if i32::from(local_addr.sa_family) == AF_INET {
-        println!("-------------------");
-        let data = local_addr.sa_data.as_ptr() as *const u8;
-        let data_arr = (*slice_from_raw_parts(data, 14)).to_vec();
-        let port_bytes = [*data.offset(0), *data.offset(1)];
-        let port = u16::from_le_bytes(port_bytes);
-        let ip = SocketAddrV4::new(
-            Ipv4Addr::new(
-                *data.offset(1), *data.offset(3), *data.offset(4), *data.offset(5))
-            , port);
-
-        println!("udp->created() id= {}, process_id={} local_ip= {} local_address={}",
-                 id,
-                 (*conn_info).process_id,
-                 ip.to_string(),
-                 port);
-    }
+    // let local_addr: SOCKADDR = (*conn_info).local_address;
+    // if i32::from(local_addr.sa_family) == AF_INET {
+    //     println!("-------------------");
+    //     let data = local_addr.sa_data.as_ptr() as *const u8;
+    //     let data_arr = (*slice_from_raw_parts(data, 14)).to_vec();
+    //     let port_bytes = [*data.offset(0), *data.offset(1)];
+    //     let port = u16::from_le_bytes(port_bytes);
+    //     let ip = SocketAddrV4::new(
+    //         Ipv4Addr::new(
+    //             *data.offset(1), *data.offset(3), *data.offset(4), *data.offset(5))
+    //         , port);
+    //
+    //     println!("udp->created() id= {}, process_id={} local_ip= {} local_address={}",
+    //              id,
+    //              (*conn_info).process_id,
+    //              ip.to_string(),
+    //              port);
+    // }
 }
 
 extern fn nf_udp_connect_request(id: c_longlong, conn_info: NFUdpConnInfo) {
@@ -185,12 +180,12 @@ extern fn nf_udp_closed(id: c_longlong, conn_info: NFUdpConnInfo) {
     println!("udp->close();");
 }
 
-unsafe extern fn nf_udp_receive(id:ENDPOINT_ID, remoteAddress: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions) {
+unsafe extern fn nf_udp_receive(id: EndpointId, remoteAddress: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions) {
     println!("udp->receive();");
     nf_udpPostReceive(id, remoteAddress, buf, len, options);
 }
 
-unsafe extern fn nf_udp_send(id: ENDPOINT_ID, remoteAddress: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions) {
+unsafe extern fn nf_udp_send(id: EndpointId, remoteAddress: *const u_char, buf: *const c_char, len: c_int, options: &NFUdpOptions) {
     println!("udp->send: id={}", id);
     nf_udpPostSend(id, remoteAddress, buf, len, options);
 }
@@ -204,6 +199,12 @@ extern fn nf_udp_can_send(id: c_longlong) {
 }
 
 fn main() {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            // WriteLogger::new(LevelFilter::Info, Config::default(), File::create("my_rust_binary.log").unwrap()),
+        ]
+    ).unwrap();
     unsafe {
         let nf_driver_name = CString::new(b"netfilter2" as &[u8]).unwrap();
         let nf_driver_name_ptr: *const c_char = nf_driver_name.as_ptr();
@@ -236,15 +237,30 @@ fn main() {
             local_ip_address_mask: [0; 16],
             remote_ip_address: [0; 16],
             remote_ip_address_mask: [0; 16],
-            filtering_flag: NFFilteringFlag::NF_FILTER,
+            filtering_flag: NFFilteringFlag::NfFilter,
         };
         nf_adjustProcessPriviledges();
         // let init_status =
-        nf_init(nf_driver_name_ptr, &nf_handler);
-        let rule_status = nf_addRule(&nf_rule, 0);
-        println!("started");
+        let init_status = nf_init(nf_driver_name_ptr, &nf_handler);
+        match init_status {
+            NFStatus::NfStatusSuccess => {
+                info!("nf driver inited success! starting add rule.");
+                let add_rule_status = nf_addRule(&nf_rule, 0);
+                match add_rule_status {
+                    NFStatus::NfStatusSuccess => {
+                        info!("nf rule added! starting do filtering.");
+                    }
+                    _ => {
+                        println!("add nf rule failed! err={:?} ", add_rule_status);
+                    }
+                }
+            }
+            _ => {
+                error!("nf driver inited failed! err={:?}", init_status);
+            }
+        }
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        stdin().read_line(&mut guess).expect("Failed to read line");
         println!("You guessed: {}", guess);
         nf_free()
     };
